fc6d33028e91c2d3d90867aeabae595119b2402c
yudialfayat <ayudi3521@gmail.com>
2022-01-05 11:31:40 +0700

    Show custom palette controller

--- src/components/Controller.tsx
+++ src/components/Controller.tsx
@@ -66,7 +66,7 @@ const Controller: FC<ControllerPorps> = ({ currentState, baseURL }) => {
 			<div className="controller-container">
 				<DynamicController sendData={sendData} dynamicState={currentState.dyn} />
 				<PaletteController sendData={sendData} paletteState={currentState.pal} />
-				{/* <CustomPaletteController sendData={sendData} updateConsoleFeedback={updateFeedback} /> */}
+				<CustomPaletteController sendData={sendData} updateConsoleFeedback={updateFeedback} />
 				<div className="console feedback-container controller-item">
 					{feedbacks.map((feedback, idx) => (
 						<div key={idx} className={feedback.success ? 'feedback-item success' : 'feedback-item failed'}>
